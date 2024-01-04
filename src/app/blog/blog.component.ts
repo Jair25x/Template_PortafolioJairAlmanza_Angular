571bf60ce62dfd780d1b0159f15b0e42d0a3c7d4
Misantropo25 <019100178G@uandina.edu.pe>
2024-01-03 19:20:24 -0500

    Preparar para GitHub Pages

---
+++ src/app/blog/blog.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { CvabastidasService } from '../services/portJairAlmanza.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-blog',
+  templateUrl: './blog.component.html',
+  styleUrls: ['./blog.component.css']
+})
+export class BlogComponent implements OnInit {
+  url!: string;
+
+  constructor(public cv: CvabastidasService,
+              private sanitizer: DomSanitizer
+              ) { }
+
+  ngOnInit(): void {
+  }
+
+  getVideoIframe(url: string) {
+    let video;
+    let results;
+
+    if (url === null) {
+        return '';
+    }
+    results = url.match('[\\?&]v=([^&#]*)');
+    video = (results === null) ? url : results[1];
+
+    return this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/' + video);
+  }
+
+}
